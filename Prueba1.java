3dc7731509d5680b7ebfa550510aad94b86330e0
Ruben <22rpf2006@gmail.com>
2024-09-24 03:18:00 -0400

    Adicionalmente mostrara mi lugar favorito

---
+++ Prueba1.java
@@ -0,0 +1,6 @@
+public class Prueba1 {
+	public static void main (String [] args) {
+		System.out.println("Mi deporte favorito es el futbol.");
+		System.out.println ("Mi lugar favorito es la playa");
+	}
+}
