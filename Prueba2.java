676a8e567986eda6d412c205e2d8bde98ded04be
Ruben <22rpf2006@gmail.com>
2024-09-24 03:23:04 -0400

    Adicionalmente me mostrara mi red social favorita

---
+++ Prueba2.java
@@ -0,0 +1,7 @@
+public class Prueba2 {
+	public static void main (String [] args) {
+		System.out.println("Mi deporte favorito es el futbol.");
+		System.out.println ("Mi lugar favorito es la playa");
+		System.out.println ("Mi red social favorita es Instagram");
+	}
+}
